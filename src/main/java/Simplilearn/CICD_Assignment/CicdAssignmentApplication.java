cef7c9227cdce3953e9bbdce7ef4a7840bc14560
Sandeep0408 <sandeepkumar41993@yahoo.in>
2021-06-04 01:05:47 +0530

    Adding POM file to git repo

---
+++ src/main/java/Simplilearn/CICD_Assignment/CicdAssignmentApplication.java
@@ -0,0 +1,13 @@
+package Simplilearn.CICD_Assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdAssignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Simplilearn/CICD_Assignment/CicdAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package Simplilearn.CICD_Assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
